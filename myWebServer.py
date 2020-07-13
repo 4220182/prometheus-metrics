a54ae6b6d8cfb4960c3009a0dab90dfeb3453117
4220182 <4220182@qq.com>
2020-07-13 21:54:30 +0800

    update http_requests_total output

--- myWebServer.py
+++ myWebServer.py
@@ -5,26 +5,39 @@
 使用python内置WSGI server: wsgiref ,考虑性能问题你也可以使用其他WSGI server
 WSGI server用了gevent, eventlet等 green thread技术，就可以支持更多并发。
 """
-from prometheus_client import start_http_server, Summary, Counter
+from prometheus_client import start_http_server, Counter, Summary
 import random
 import time
 from flask import Flask, jsonify
 from wsgiref.simple_server import make_server
 
-# Create a metric to track time spent and requests made.
-REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
-Counter_TIME = Counter('My_Counter', 'My Counter Desc')
+# 定义一个Counter类型的变量，这个变量不是指标名称，这种Counter类型只增加
+# 不减少，程序重启的时候会被重新设置为0，构造函数第一个参数是定义 指标名称，
+# 第二个是定义HELP中显示的内容，都属于文本
+# 第三个参数是标签列表，也就是给这个指标加labels，这个也可以不设置
+http_requests_total = Counter("http_requests", "Total request count of the host", ['method', 'endpoint'])
+
+# Summary类型，它可以统计2个时间
+# request_processing_seconds_count 该函数被调用的数量
+# request_processing_seconds_sum  该函数执行所花的时长
+request_time = Summary('request_processing_seconds', 'Time spent processing request')
 
 app = Flask(__name__)
 
-
 # Decorate function with metric.
-@app.route("/test1")
-@REQUEST_TIME.time()
+@app.route("/")
+@request_time.time() # 这个必须要放在app.route的下面
 def process_request():
-    """A dummy function that takes some time."""
+
+    # 这里设置0-1之间随机数用于模拟页面响应时长
     time.sleep(random.random())
-    Counter_TIME.inc()
+
+    # .inc()表示增加，默认是加1，你可以设置为加1.5，比如.inc(1.5)
+    # http_requests_total.inc()
+    # 下面这种写法就是为这个指标加上标签，但是这里的method和endpoint
+    # 都在Counter初始化的时候放进去的。
+    # 你想统计那个ULR的访问量就把这个放在哪里
+    http_requests_total.labels(method="get", endpoint="/").inc()
     return jsonify({"return": "success OK!"})
 
 
